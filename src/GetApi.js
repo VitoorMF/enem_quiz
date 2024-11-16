11c984efdcfa3e35d4dba311a9a4cdb4f09e6af5
Vitor MF <vitorfreire419@gmail.com>
2024-11-16 18:12:08 -0300

    image

--- src/GetApi.js
+++
@@ -1,6 +0,0 @@
-const options = {method: 'GET'};
-
-fetch('https://api.enem.dev/v1/exams/{year}/questions', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
